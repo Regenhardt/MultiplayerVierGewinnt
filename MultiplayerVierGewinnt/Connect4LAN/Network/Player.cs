fb2704b0575b5f346f5bad0d294163beac9f6ec2
Laptop <Laptop@flaptop.com>
2017-11-03 20:57:08 +0100

    implemented socket logic for player

--- MultiplayerVierGewinnt/Connect4LAN/Network/Player.cs
+++ MultiplayerVierGewinnt/Connect4LAN/Network/Player.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
@@ -16,6 +18,33 @@ namespace Connect4LAN.Network
         public Color Color { get; private set; }
         public string Name { get; private set; }
         public IPAddress IP { get; private set; }
+
+        private TcpClient connectionSocket;
+        private StreamReader @in;
+        private StreamWriter @out;
+
+        /// <summary>
+        /// The Socket with wich this player is connected
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/> 
+        public TcpClient Socket
+        {
+            get { return connectionSocket; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+
+                if (value != connectionSocket)
+                {
+                    connectionSocket = value;
+                    //reset the streams
+                    @in = new StreamReader(connectionSocket.GetStream());
+                    @out = new StreamWriter(connectionSocket.GetStream());
+                }
+            }
+        }
+
 
         public Player()
         {
