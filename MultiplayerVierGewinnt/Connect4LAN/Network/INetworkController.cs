747691a8a31179325b5d0edb9e1e8097e3bb9296
Marlon Regenhardt <marlon.r@hotmail.de>
2017-11-03 10:55:10 +0100

    NetworkController Interface

---
+++ MultiplayerVierGewinnt/Connect4LAN/Network/INetworkController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4LAN.Network
+{
+	internal interface INetworkController
+	{
+		
+		/// <summary>
+		/// EventArgs object contains the received JSON-string
+		/// </summary>
+		event EventHandler<string> Received;
+
+		/// <summary>
+		/// Indicates a critical loss of connection that couldn't be recovered.
+		/// </summary>
+		event EventHandler ConnectionLost;
+
+		/// <summary>
+		/// Connects to the given IP.
+		/// </summary>
+		/// <param name="ipAddress">The Address to connect to.</param>
+		/// <returns>Whether or not the connection was successful.</returns>
+		bool Connect(string ipAddress);
+
+		/// <summary>
+		/// Returns IP Addresses of available Servers.
+		/// </summary>
+		/// <returns>IP Addresses of available Servers</returns>
+		IEnumerable<string> GetAvailableConnections();
+
+		/// <summary>
+		/// Stops the connection.
+		/// </summary>
+		void Disconnect();
+	}
+}
