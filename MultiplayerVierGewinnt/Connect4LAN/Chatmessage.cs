49ab7585d1c6ff0e5e9d6790e0406f442da0d03c
Laptop <Laptop@flaptop.com>
2017-11-05 15:11:55 +0100

    Added and implemented a Chatmessage to make parsing easier lateron

---
+++ MultiplayerVierGewinnt/Connect4LAN/Chatmessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4LAN
+{
+	struct Chatmessage
+	{
+		public string Message { get; set; }
+		public string From { get; set; }
+		public DateTime Sent { get; set; }
+	
+
+		public Chatmessage(string msg, string from)
+		{
+			this.Message = msg;
+			this.From = from;
+
+			Sent = DateTime.Now;
+		}
+	}
+}
