f095dcf6947c3876541930e3b5aedd3ca12f3321
Laptop <Laptop@flaptop.com>
2017-11-05 12:46:31 +0100

    Added Unit Test Project

---
+++ MultiplayerVierGewinnt/Connect4LAN/Network/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4LAN.Network
+{
+#if DEBUG
+	/// <summary>
+	/// Wrapper for unit tests -> don'T have to declare other classes public
+	/// </summary>
+	public class Test
+	{
+		public Test()
+		{
+
+		}
+	}
+#endif
+}
---
+++ MultiplayerVierGewinnt/Connect4LANTests/Network/ServerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Connect4LAN.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4LAN.Network.Tests
+{
+	[TestClass()]
+	public class ServerTests
+	{
+		[TestMethod()]
+		public void ServerTest()
+		{
+
+		}
+	}
+}
