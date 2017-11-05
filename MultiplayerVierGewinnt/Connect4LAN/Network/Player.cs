bb20b5d8d5df4294cb0ef4e45c3a1cc5810a9521
Laptop <Laptop@flaptop.com>
2017-11-05 10:02:33 +0100

    sorted and organized the class

--- MultiplayerVierGewinnt/Connect4LAN/Network/Player.cs
+++ MultiplayerVierGewinnt/Connect4LAN/Network/Player.cs
@@ -14,23 +14,54 @@ namespace Connect4LAN.Network
     /// Class representing a Player
     /// </summary>
     class Player : INetworkController
-    {
-        public Color Color { get; private set; }
-        public string Name { get; private set; }
-        public IPAddress IP { get; private set; }
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="color">The Color of the Player</param>
+		/// <param name="name">The name of the Player</param>
+		/// <param name="ip">The IP with wich he is connected</param>
+		/// <param name="socket">The Socket on wich e is connected</param>
+		/// <exception cref="ArgumentNullException"/>
+		public Player(Color color, string name, IPAddress ip, TcpClient socket)
+		{
+			this.Color = color;
+			this.Name = name;
+			this.IP = ip;
+			this.Socket = socket;
+		}
 
-        private TcpClient connectionSocket;
-        private StreamReader @in;
-        private StreamWriter @out;
+		/// <summary>
+		/// Sends a Message to the player
+		/// </summary>
+		/// <param name="msg"></param>
+		public void SendMessage(string msg)
+		{
+			@out.WriteLine(msg);
+		}
 
-        public event EventHandler<string> Received;
-        public event EventHandler ConnectionLost;
+		/// <summary>
+		/// Returns the last message from the player
+		/// </summary>
+		/// <returns></returns>
+		public string ReadLastMessage()
+		{
+			return lastMesssage;
+		}
 
-        /// <summary>
-        /// The Socket with wich this player is connected
-        /// </summary>
-        /// <exception cref="ArgumentNullException"/> 
-        public TcpClient Socket
+
+		#region  [ Properties ]
+
+		public Color Color { get; private set; }
+		public string Name { get; private set; }
+		public IPAddress IP { get; private set; }
+
+		
+		/// <summary>
+		/// The Socket with wich this player is connected
+		/// </summary>
+		/// <exception cref="ArgumentNullException"/> 
+		public TcpClient Socket
         {
             get { return connectionSocket; }
             set
@@ -44,53 +75,81 @@ namespace Connect4LAN.Network
                     //reset the streams
                     @in = new StreamReader(connectionSocket.GetStream());
                     @out = new StreamWriter(connectionSocket.GetStream());
+					@out.AutoFlush = true;
+
+					//loop for listenning for incoming messages
+					Task.Factory.StartNew(() =>
+					{
+						//exception is thrown when connection is lost
+						try
+						{
+							while(connectionSocket.Connected)
+							{
+								//read the message and send out the recieved message
+								lastMesssage = @in.ReadLine();
+								Received?.Invoke(this, lastMesssage);
+							}
+						}
+						catch (IOException)
+						{
+							//Person disconnected
+							ConnectionLost?.Invoke(this, EventArgs.Empty);
+						}
+						
+					});
                 }
             }
         }
 
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="color"></param>
-        /// <param name="name"></param>
-        /// <param name="ip"></param>
-        /// <param name="sck"></param>
-        /// <exception cref="ArgumentNullException"/>
-        public Player(Color color, string name, IPAddress ip, TcpClient sck)
-        {
-            this.Color = color;
-            this.Name = name;
-            this.IP = ip;
-            this.Socket = sck;
-        }
+		#endregion
 
-      
-        public bool Connect(string ipAddress, int port)
-        {
-            //try to connect tot the socket
-            try
-            {
-                Socket.Connect(ipAddress, port);
-                return Socket.Connected;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
+		#region [ Fields ]
 
-        public IEnumerable<string> GetAvailableConnections()
-        {
-            throw new NotImplementedException();
-        }
+		private TcpClient connectionSocket;
+		private StreamReader @in;
+		private StreamWriter @out;
+		private string lastMesssage;
 
-        /// <summary>
-        /// Closes the Socket
-        /// </summary>
-        public void Disconnect()
-        {
-            Socket.Close();
-        }
-    }
+		#endregion
+
+		#region [ Events ]
+
+		public event EventHandler<string> Received;
+		public event EventHandler ConnectionLost;
+
+		#endregion
+
+		#region [ INetworkController Members ]
+
+		public bool Connect(string ipAddress, int port)
+		{
+			//try to connect tot the socket
+			try
+			{
+				Socket.Connect(ipAddress, port);
+				return Socket.Connected;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public IEnumerable<string> GetAvailableConnections()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Closes the Socket
+		/// </summary>
+		public void Disconnect()
+		{
+			Socket.Close();
+		}
+
+		#endregion [ INetworkController Members ]
+
+	}
 }
