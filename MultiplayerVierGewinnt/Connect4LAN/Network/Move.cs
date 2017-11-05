80a72d0f1063f3c2a81b54624ff42a6c85f46634
Laptop <Laptop@flaptop.com>
2017-11-05 15:13:07 +0100

    Added struct "move"

---
+++ MultiplayerVierGewinnt/Connect4LAN/Network/Move.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4LAN.Network
+{
+	/// <summary>
+	/// The move a player made
+	/// </summary>
+	struct Move
+	{
+
+	}
+}
