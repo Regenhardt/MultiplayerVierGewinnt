dc33a83fdb9fe0508c85c036bbf2be8d305f61c0
Marlon Regenhardt <marlon.regenhardt@hotmail.de>
2017-11-08 13:32:13 +0100

    View except game grid visuals

---
+++ MultiplayerVierGewinnt/Connect4LAN/View/InputBox.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Connect4LAN.View
+{
+    /// <summary>
+    /// Interaction logic for InputBox.xaml
+    /// </summary>
+    public partial class InputBox : UserControl
+    {
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+        public ICommand @Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(
+                "Command", 
+                typeof(ICommand), 
+                typeof(InputBox), 
+                new UIPropertyMetadata(null));
+    }
+}
--- MultiplayerVierGewinnt/Connect4LAN/View/MainWindow.xaml.cs
+++ MultiplayerVierGewinnt/Connect4LAN/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Connect4LAN
+namespace Connect4LAN.View
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
