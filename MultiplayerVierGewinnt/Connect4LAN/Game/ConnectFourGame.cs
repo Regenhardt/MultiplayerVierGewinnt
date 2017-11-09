b956831a8fc79d3da91fb6902c2249409477ce02
Laptop <Laptop@flaptop.com>
2017-11-09 14:51:14 +0100

    Added logic for connection

--- MultiplayerVierGewinnt/Connect4LAN/Game/ConnectFourGame.cs
+++ MultiplayerVierGewinnt/Connect4LAN/Game/ConnectFourGame.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 namespace Connect4LAN.Game
 {
     /// <summary>
-    /// Represents a Connect4 Game instance
+    /// Represents a Connect4 Game instance.
     /// </summary>
     class ConnectFourGame
     {
@@ -18,6 +18,9 @@ namespace Connect4LAN.Game
         public Player player2 { get; private set; }
 
 		public Gameboard Gameboard { get; private set; }
+
+		//Flag to dermin wether we ahve a winner
+		private bool weHaveAWinner = false;
 
 		/// <summary>
 		/// Instaniates a Game of Connect 
@@ -31,9 +34,47 @@ namespace Connect4LAN.Game
             this.player2 = player2;
 			this.Gameboard = new Gameboard();
 
-			//listen to moves by the player
-			player1.NetworkAdapter.Received += (s, e) => { if (e.MessageType == NetworkMessageType.Move) placePiece((Move)e.Message, player1.Color); };
-			player2.NetworkAdapter.Received += (s, e) => { if (e.MessageType == NetworkMessageType.Move) placePiece((Move)e.Message, player2.Color); };
+			EventHandler<NetworkMessage> handler;
+
+			//wire up player 1
+			handler = (s, e) => { if (e.MessageType == NetworkMessageType.Move) executeMove((Move)e.Message, player1); };
+			player1.NetworkAdapter.Received += handler;
+			player1.NetworkAdapter.ConnectionLost += (s, e) => {
+				player2.NetworkAdapter.SendMessage($"{player2.Name} lost connection.{((weHaveAWinner)?"You won by elimination." : "" )}", NetworkMessageType.ServerMessage);
+				player1.NetworkAdapter.Received -= handler;
+				player1.NetworkAdapter.Disconnect();
+			};
+
+			//wire up player 2
+			handler = (s, e) => { if (e.MessageType == NetworkMessageType.Move) executeMove((Move)e.Message, player2); };
+			player2.NetworkAdapter.Received += handler;
+			player1.NetworkAdapter.ConnectionLost += (s, e) => {
+				player2.NetworkAdapter.SendMessage($"{player1.Name} lost connection.{((weHaveAWinner)?"You won by elimination." : "" )}", NetworkMessageType.ServerMessage);
+				player2.NetworkAdapter.Received -= handler;
+				player2.NetworkAdapter.Disconnect();
+			};
+		}
+
+		/// <summary>
+		/// Writes the message to both players
+		/// </summary>
+		/// <param name="msg"></param>
+		/// <param name="type"></param>
+		private void writeMessageToPlayers(string msg, NetworkMessageType type = NetworkMessageType.ServerMessage)
+		{
+			this.player1.NetworkAdapter.SendMessage(msg, type);
+			this.player2.NetworkAdapter.SendMessage(msg, type);
+		}
+
+		private void executeMove(Move move, Player player)
+		{
+			//place his piece
+			var piece = placePiece(move, player.Color);
+			if (isWinningMove(piece))
+			{
+				string msg = $"Player {player.Name} has won.";
+				weHaveAWinner = true;
+			}
 		}
 
 		/// <summary>
@@ -41,21 +82,32 @@ namespace Connect4LAN.Game
 		/// </summary>
 		/// <param name="move"></param>
 		/// <param name="color"></param>
-		private void placePiece(Move move, Color color)
+		private Piece placePiece(Move move, Color color)
 		{
 			Piece piece = new Piece{ Color = color };
 			this.Gameboard.PutPiece(move.Column, piece);
-
-			checkIfPlayerWon(piece);
+			return piece;			
 		}
 
 		/// <summary>
 		/// Checks if the color of this piece has connected 4.
 		/// </summary>
 		/// <param name="piece"></param>
-		private void checkIfPlayerWon(Piece piece)
+		private bool isWinningMove(Piece piece)
 		{
-			throw new NotImplementedException();
+			int[] allProps = new int[]
+			{
+				piece.FriendlyAmountBottom,
+				piece.FriendlyAmountBottomLeft,
+				piece.FriendlyAmountBottomRight,
+				piece.FriendlyAmountMiddle,
+				piece.FriendlyAmountMiddleLeft,
+				piece.FriendlyAmountMiddleRight,
+				piece.FriendlyAmountTopLeft,
+				piece.FriendlyAmountTopRight
+			};
+
+			return allProps.Any(i => i >= 4);
 		}
 	}    
 }
