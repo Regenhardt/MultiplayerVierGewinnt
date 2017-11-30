f3da42569229ab150342d81700345e12259a4b56
Marlon Regenhardt <marlon.regenhardt@hotmail.de>
2017-11-30 01:14:24 +0100

    Created server project, View and ViewModel

---
+++ MultiplayerVierGewinnt/Connect4LanServer/DedicatedServerViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4LanServer
+{
+	public class DedicatedServerViewModel : INotifyPropertyChanged
+	{
+		#region [ Properties ]
+
+
+		public ObservableCollection<object> Games
+		{
+			get
+			{
+				return games;
+			}
+			set
+			{
+				games = value;
+				Notify();
+			}
+		}
+		private ObservableCollection<object> games;
+
+		#endregion
+
+		#region [ NotifyPropertyChanged ]
+
+		private void Notify([CallerMemberName]string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		private void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+		{
+			Notify(propertyName);
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+	}
+}
