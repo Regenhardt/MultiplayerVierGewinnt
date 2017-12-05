1408c137aa417326ed5e5803b7f81f25e3d8bdc7
Marlon Regenhardt <marlon.regenhardt@hotmail.de>
2017-12-05 09:46:53 +0100

    Populated LobbyCommunications.

--- MultiplayerVierGewinnt/Connect4LanServer/Network/LobbyCommunications.cs
+++ MultiplayerVierGewinnt/Connect4LanServer/Network/LobbyCommunications.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Connect4LanServer.Network
 {
+	/// <summary>
+	/// Holds the different types of communication between client and Server before the game starts.
+	/// </summary>
 	enum LobbyCommunications
 	{
+		/// <summary>
+		/// The Game starts now.
+		/// No payload.
+		/// </summary>
+		GameStarted,
+		/// <summary>
+		/// Broadcast to find running dedicated server.
+		/// No payload.
+		/// </summary>
+		Discover,
+		/// <summary>
+		/// Queries the server for available lobbies.
+		/// Payload is <see cref="Dictionary{int, string}"/>
+		/// </summary>
+		AvailableLobbies,
+		/// <summary>
+		/// Client wants to join a particular lobby.
+		/// Payload is int of the player to join.
+		/// </summary>
+		JoinLobby,
+		/// <summary>
+		/// Client wants to create a new lobby.
+		/// No payload.
+		/// </summary>
+		CreateLobby
 	}
 }
