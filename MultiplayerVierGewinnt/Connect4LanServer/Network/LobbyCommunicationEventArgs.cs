8804722f79b4c24d1f63a4fb3f5b533d99dd38c6
Laptop <Laptop@flaptop.com>
2017-12-05 09:22:40 +0100

    added base logic for LobbyCommunication

---
+++ MultiplayerVierGewinnt/Connect4LanServer/Network/LobbyCommunicationEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4LanServer.Network
+{
+	class LobbyCommunicationEventArgs : EventArgs
+	{
+		public LobbyCommunicationEventArgs(LobbyCommunications type, dynamic data)
+		{
+			this.LobbyCommunicationType = type;
+			this.Data = data;
+		}
+
+		public LobbyCommunications LobbyCommunicationType { get; private set; }
+
+		public dynamic Data { get; private set; }
+	}
+}
